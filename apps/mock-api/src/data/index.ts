57cad2c4538d0f883d076c5909270c3e227784da
Paul Kilmurray <paul@kilbot.com>
2026-02-25 11:20:26 +0100

    feat(mock-api): add order and customer mock data

--- apps/mock-api/src/data/index.ts
+++ apps/mock-api/src/data/index.ts
@@ -1,2 +1,4 @@
 export type { NeutralProduct, NeutralOrder, NeutralCustomer, NeutralAddress } from './types';
 export { products } from './catalog';
+export { orders } from './orders';
+export { customers } from './customers';
